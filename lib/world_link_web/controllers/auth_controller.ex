00b6e8e2a826f14921b6f5d3caec6a95f0427a7a
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-03-09 16:47:16 +0700

    Add auth_controller

--- lib/world_link_web/controllers/auth_controller.ex
+++ lib/world_link_web/controllers/auth_controller.ex
@@ -1,16 +1,26 @@
 defmodule WorldLinkWeb.AuthController do
+  alias WorldLink.Identity
   use WorldLinkWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    IO.puts("### Conn")
-    conn
-    |> dbg()
+  action_fallback WorldLinkWeb.FallbackController
 
-    IO.puts("### Params")
-    params
-    |> IO.inspect()
+  def callback(conn, _params) do
+    assigns = conn.assigns.ueberauth_auth
+    user_params = build_params_based_on_provider(assigns.provider, assigns)
 
-    conn
+    case Identity.create_oauth_user(user_params) do
+      {:ok, user} -> render(conn, "show.json", user: user)
+    end
+  end
+
+  defp build_params_based_on_provider(:discord, params) do
+    %{
+      provider_uid: params.uid,
+      oauth_provider: :discord,
+      name: params.info.name || params.info.nickname,
+      email: params.info.email,
+      avatar: params.info.image
+    }
   end
 end

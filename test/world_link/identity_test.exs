04d6ed949c7b3f805e24c110505531920c3e5e1f
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-03-21 22:45:27 +0700

    Adjust tests

--- test/world_link/identity_test.exs
+++ test/world_link/identity_test.exs
@@ -12,32 +12,36 @@ defmodule WorldLink.IdentityTest do
     @invalid_attrs %{
       activated: nil,
       activated_at: nil,
-      auth_token: nil,
-      auth_token_expires_at: nil,
       email: "some email",
-      handle: nil,
       name: nil,
-      provider_uid: nil,
-      uuid: nil,
-      password: nil,
-      oauth_provider: nil
+      password: nil
     }
 
     test "create_user/1 with valid data creates a user" do
       valid_attrs = %{
         email: "sam@doe.com",
         name: "some name",
-        password: "somepassword"
+        password: "somepassword",
+        nickname: "some_nickname"
       }
 
       assert {:ok, %User{} = user} = Identity.create_user(valid_attrs)
       assert user.email == "sam@doe.com"
       assert user.name == "some name"
-      assert user.uuid
+      assert user.nickname == "some_nickname"
+      assert user.id
     end
 
     test "create_user/1 with invalid data returns error changeset" do
       assert {:error, %Ecto.Changeset{}} = Identity.create_user(@invalid_attrs)
     end
+  end
+
+  describe "oauth_profiles" do
+    alias WorldLink.Identity.OauthProfile
+
+    import WorldLink.IdentityFixtures
+
+    @invalid_attrs %{oauth_provider: nil}
   end
 end

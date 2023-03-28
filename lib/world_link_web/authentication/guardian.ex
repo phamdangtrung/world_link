38e8675db1d0432f5fc720c64b60a10c5331143c
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-03-28 10:22:22 +0700

    Add Reauthentication

--- lib/world_link_web/authentication/guardian.ex
+++ lib/world_link_web/authentication/guardian.ex
@@ -12,10 +12,6 @@ defmodule WorldLinkWeb.Authentication.Guardian do
 
   def subject_for_token(_, _), do: {:error, :reason_for_error}
 
-  # def build_claims(claims, _, _) do
-  #   {:ok, claims} = build_claims(%{role: "admin"}, nil, nil)
-  #   {:ok, claims}
-  # end
 
   def resource_from_claims(%{"sub" => id}) do
     case Identity.get_user!(id) do
@@ -28,7 +24,9 @@ defmodule WorldLinkWeb.Authentication.Guardian do
 
   def authenticate(email, password) do
     case Identity.get_user_by_email(email) do
-      nil -> {:error, :not_found}
+      nil ->
+        {:error, :not_found}
+
       user ->
         Identity.verify_password(user, password)
         |> case do
@@ -39,16 +37,58 @@ defmodule WorldLinkWeb.Authentication.Guardian do
   end
 
   def verify_access_token(token) do
-    decode_and_verify(token, %{typ: "access", max_age: {@access_token_ttl_in_hours, :hour}, verify_issuer: true})
+    decode_and_verify(
+      token,
+      %{
+        typ: "access"
+      },
+      verify_issuer: true
+    )
   end
 
   def verify_refresh_token(token) do
-    decode_and_verify(token, %{typ: "refresh", max_age: {@refresh_token_ttl_in_days, :day}, verify_issuer: true})
+    decode_and_verify(
+      token,
+      %{
+        typ: "refresh"
+      },
+      verify_issuer: true
+    )
   end
 
   defp create_token(user) do
-    {:ok, access_token, _claims} = encode_and_sign(user, %{role: user.role_name}, [ttl: {@access_token_ttl_in_hours, :hour}, token_type: "access", verify_issuer: true, auth_time: true])
-    {:ok, refresh_token, _claims} = encode_and_sign(user, %{}, [ttl: {@refresh_token_ttl_in_days, :day}, token_type: "refresh", verify_issuer: true, auth_time: true])
-    {:ok, access_token: access_token, refresh_token: refresh_token}
+    {:ok, access_token: create_access_token(user), refresh_token: create_refresh_token(user)}
+  end
+
+  defp create_access_token(user) do
+    {:ok, access_token, _claims} =
+      encode_and_sign(user, %{role: user.role_name},
+        ttl: {@access_token_ttl_in_hours, :hour},
+        token_type: "access",
+        verify_issuer: true,
+        auth_time: true
+      )
+
+    access_token
+  end
+
+  defp create_refresh_token(user) do
+    {:ok, refresh_token, _claims} =
+      encode_and_sign(user, %{},
+        ttl: {@refresh_token_ttl_in_days, :day},
+        token_type: "refresh",
+        verify_issuer: true,
+        auth_time: true
+      )
+
+    refresh_token
+  end
+
+  def re_authenticate(refresh_token) do
+    case(verify_refresh_token(refresh_token)) do
+      {:ok, claims} -> {:ok, resource} = resource_from_claims(claims)
+        create_token(resource)
+      {:error, reason} -> {:error, reason}
+    end
   end
 end

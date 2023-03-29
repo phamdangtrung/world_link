5de63207c6b9b706f4f9b1685acab8958804bfc9
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-03-29 11:27:49 +0700

    add user_token view

--- lib/world_link_web/views/auth_view.ex
+++ lib/world_link_web/views/auth_view.ex
@@ -1,8 +1,11 @@
 defmodule WorldLinkWeb.AuthView do
   use WorldLinkWeb, :view
 
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, __MODULE__, "user.json", as: :user)}
+  def render("token.json", %{tokens: jwt_tokens}) do
+    %{
+      access: jwt_tokens.access,
+      refresh: jwt_tokens.refresh
+    }
   end
 
   def render("user.json", %{user: user}) do
@@ -15,5 +18,13 @@ defmodule WorldLinkWeb.AuthView do
       inserted_at: user.inserted_at,
       updated_at: user.updated_at
     }
+  end
+
+  def render("user_token.json", %{tokens: jwt_tokens}) do
+    %{data: render_one(jwt_tokens, __MODULE__, "token.json", as: :tokens)}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, __MODULE__, "user.json", as: :user)}
   end
 end

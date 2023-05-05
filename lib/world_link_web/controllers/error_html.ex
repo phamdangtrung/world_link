388d60933420e0ce9ab441ffb0c108a0b89d92bc
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-05-05 16:41:25 +0700

    Add example error template

--- lib/world_link_web/controllers/error_html.ex
+++ lib/world_link_web/controllers/error_html.ex
@@ -13,6 +13,8 @@ defmodule WorldLinkWeb.ErrorHTML do
   # The default is to render a plain text page based on
   # the template name. For example, "404.html" becomes
   # "Not Found".
+  embed_templates "error_html/*"
+
   def render(template, _assigns) do
     Phoenix.Controller.status_message_from_template(template)
   end
--- lib/world_link_web/controllers/error_json.ex
+++ lib/world_link_web/controllers/error_json.ex
@@ -9,6 +9,10 @@ defmodule WorldLinkWeb.ErrorJSON do
   # By default, Phoenix returns the status message from
   # the template name. For example, "404.json" becomes
   # "Not Found".
+  def render("500.json", _assigns) do
+    %{errors: %{detail: "Internal Server Error occurred!"}}
+  end
+
   def render(template, _assigns) do
     %{errors: %{detail: Phoenix.Controller.status_message_from_template(template)}}
   end

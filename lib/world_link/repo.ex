77ed67600789aceb0eecd899921294898e887ce3
Pham Dang Trung <55026963+phamdangtrung@users.noreply.github.com>
2023-05-08 15:35:37 +0700

    Refactor Repo

--- lib/world_link/repo.ex
+++ lib/world_link/repo.ex
@@ -6,11 +6,11 @@ defmodule WorldLink.Repo do
   def init(_, config) do
     config =
       config
-      |> Keyword.put(:username, "postgres")
-      |> Keyword.put(:password, "postgres")
-      |> Keyword.put(:database, "wl_development")
-      |> Keyword.put(:hostname, "db")
-      |> Keyword.put(:port, "5432")
+      |> Keyword.put(:username, System.get_env("PGUSER"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD"))
+      |> Keyword.put(:database, System.get_env("PGDATABASE"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST"))
+      |> Keyword.put(:port, System.get_env("PGPORT"))
 
     {:ok, config}
   end
